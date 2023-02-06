4903d915d1814f739a0f170fb0f9d8e4deecafca
Filleux <69526770+FilleuxStudio@users.noreply.github.com>
2023-02-06 23:27:20 +0100

    init

---
+++ PngToIcoImageConverter/FRM_Main.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PngToIcoImageConverter
+{
+    partial class FRM_Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRM_Main));
+            this.TXT_Source = new System.Windows.Forms.TextBox();
+            this.BT_OpenFolderDest = new System.Windows.Forms.Button();
+            this.BT_SetSource = new System.Windows.Forms.Button();
+            this.BT_Convert = new System.Windows.Forms.Button();
+            this.LBL_title = new System.Windows.Forms.Label();
+            this.LBL_Message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TXT_Source
+            // 
+            this.TXT_Source.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TXT_Source.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TXT_Source.Location = new System.Drawing.Point(52, 51);
+            this.TXT_Source.Name = "TXT_Source";
+            this.TXT_Source.Size = new System.Drawing.Size(559, 22);
+            this.TXT_Source.TabIndex = 0;
+            // 
+            // BT_OpenFolderDest
+            // 
+            this.BT_OpenFolderDest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BT_OpenFolderDest.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BT_OpenFolderDest.BackgroundImage")));
+            this.BT_OpenFolderDest.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BT_OpenFolderDest.FlatAppearance.BorderSize = 0;
+            this.BT_OpenFolderDest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BT_OpenFolderDest.Location = new System.Drawing.Point(16, 189);
+            this.BT_OpenFolderDest.Name = "BT_OpenFolderDest";
+            this.BT_OpenFolderDest.Size = new System.Drawing.Size(74, 74);
+            this.BT_OpenFolderDest.TabIndex = 2;
+            this.BT_OpenFolderDest.UseVisualStyleBackColor = true;
+            this.BT_OpenFolderDest.Click += new System.EventHandler(this.BT_OpenFolderDest_Click);
+            // 
+            // BT_SetSource
+            // 
+            this.BT_SetSource.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BT_SetSource.BackgroundImage")));
+            this.BT_SetSource.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BT_SetSource.FlatAppearance.BorderSize = 0;
+            this.BT_SetSource.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BT_SetSource.Location = new System.Drawing.Point(12, 44);
+            this.BT_SetSource.Name = "BT_SetSource";
+            this.BT_SetSource.Size = new System.Drawing.Size(34, 34);
+            this.BT_SetSource.TabIndex = 1;
+            this.BT_SetSource.UseVisualStyleBackColor = true;
+            this.BT_SetSource.Click += new System.EventHandler(this.BT_SetSource_Click);
+            // 
+            // BT_Convert
+            // 
+            this.BT_Convert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BT_Convert.Font = new System.Drawing.Font("Trebuchet MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_Convert.Location = new System.Drawing.Point(454, 89);
+            this.BT_Convert.Name = "BT_Convert";
+            this.BT_Convert.Size = new System.Drawing.Size(157, 32);
+            this.BT_Convert.TabIndex = 3;
+            this.BT_Convert.Text = "Convert";
+            this.BT_Convert.UseVisualStyleBackColor = true;
+            this.BT_Convert.Click += new System.EventHandler(this.BT_Convert_Click);
+            // 
+            // LBL_title
+            // 
+            this.LBL_title.AutoSize = true;
+            this.LBL_title.Font = new System.Drawing.Font("Trebuchet MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_title.Location = new System.Drawing.Point(54, 18);
+            this.LBL_title.Name = "LBL_title";
+            this.LBL_title.Size = new System.Drawing.Size(135, 23);
+            this.LBL_title.TabIndex = 4;
+            this.LBL_title.Text = "Select picture :";
+            // 
+            // LBL_Message
+            // 
+            this.LBL_Message.AutoSize = true;
+            this.LBL_Message.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_Message.ForeColor = System.Drawing.Color.Black;
+            this.LBL_Message.Location = new System.Drawing.Point(300, 164);
+            this.LBL_Message.Name = "LBL_Message";
+            this.LBL_Message.Size = new System.Drawing.Size(0, 20);
+            this.LBL_Message.TabIndex = 5;
+            // 
+            // FRM_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(632, 273);
+            this.Controls.Add(this.LBL_Message);
+            this.Controls.Add(this.LBL_title);
+            this.Controls.Add(this.BT_Convert);
+            this.Controls.Add(this.BT_OpenFolderDest);
+            this.Controls.Add(this.BT_SetSource);
+            this.Controls.Add(this.TXT_Source);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FRM_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Format Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TXT_Source;
+        private System.Windows.Forms.Button BT_SetSource;
+        private System.Windows.Forms.Button BT_OpenFolderDest;
+        private System.Windows.Forms.Button BT_Convert;
+        private System.Windows.Forms.Label LBL_title;
+        private System.Windows.Forms.Label LBL_Message;
+    }
+}
+
---
+++ PngToIcoImageConverter/FRM_Main.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace PngToIcoImageConverter
+{
+    public partial class FRM_Main : Form
+    {
+
+        string FolderMyPictures = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        int[] imageSizes = new int[] { 8, 16, 32, 48, 64, 72, 128, 256, 512 };
+
+        ImageInformation imageInformation;
+        public FRM_Main()
+        {
+            InitializeComponent();
+            imageInformation = new ImageInformation();
+        }
+
+        private void BT_SetSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog()
+            {
+                Filter = "Image Files|*.png;",
+                Multiselect = false,
+                Title = "Select picture",
+            };
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                TXT_Source.Text = openFile.FileName;
+
+                imageInformation.FileName = Path.GetFileName(openFile.FileName);
+                imageInformation.FilePath = openFile.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Methode qui permet de convertir une image en png vers une icon
+        /// </summary>
+        /// <param name="path">Chemin du fichier source</param>
+        /// <param name="nameFile">Nom du fichier source</param>
+        /// <returns>Bool</returns>
+        private bool ConvertPicturePngToIco(string path, string nameFile)
+        {
+            var images = new Icon[imageSizes.Length];
+
+            for (int i = 0; i < imageSizes.Length; i++)
+            {
+                using (var originalImage = Image.FromFile(path))
+                {
+                    using (var resizedImage = new Bitmap(originalImage, new Size(imageSizes[i], imageSizes[i])))
+                    {
+                        images[i] = Icon.FromHandle(resizedImage.GetHicon());
+                    }
+                }
+            }
+
+            try
+            {
+                string icoFilePath = FolderMyPictures + "\\" + nameFile.Substring(0, nameFile.Length - 4) + ".ico";
+
+                using (var originalImage = Image.FromFile(path))
+                {
+                    var resizedImage = new Bitmap(originalImage, new Size(originalImage.Width, originalImage.Height));
+                    resizedImage.MakeTransparent(Color.Transparent);
+                    var icon = Icon.FromHandle(resizedImage.GetHicon());
+                 
+
+                    using (var iconOutputStream = System.IO.File.Create(icoFilePath))
+                    {
+                        icon.Save(iconOutputStream);
+                    }
+                }
+
+                //using (var originalImage = Image.FromFile(path))
+                //{
+                //    using (var icon = Icon.FromHandle(((Bitmap)originalImage).GetHicon()))
+                //    {
+                //        using (var iconOutputStream = System.IO.File.Create(icoFilePath))
+                //        {
+                //            icon.Save(iconOutputStream);
+                //        }
+                //    }
+                //}
+
+                //using (var iconFileStream = new FileStream(icoFilePath, FileMode.Create))
+                //{
+                //    using (var iconWriter = new BinaryWriter(iconFileStream))
+                //    {
+                //        // Écriture de l'en-tête du fichier icône
+                //        iconWriter.Write((short)0);
+                //        iconWriter.Write((short)1);
+                //        iconWriter.Write((short)images.Length);
+
+                //        // Écriture des entrées pour chaque image
+                //        foreach (var image in images)
+                //        {
+                //            var imageData = GetBytes(image);
+                //            iconWriter.Write(image.Width);
+                //            iconWriter.Write(image.Height);
+                //            iconWriter.Write((byte)0);
+                //            iconWriter.Write((byte)0);
+                //            iconWriter.Write((short)1);
+                //            iconWriter.Write((short)32);
+                //            iconWriter.Write(imageData.Length);
+                //            iconWriter.Write(imageData.Length);
+                //            iconWriter.Write(0);
+                //            iconWriter.Write(0);
+                //            iconWriter.Write(imageData);
+                //        }
+                //    }
+                //}
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// Methode qui permet de convertir un icon en octet
+        /// retourne un tableau de bytes
+        /// </summary>
+        /// <param name="icon">Icon</param>
+        /// <returns>Bytes array</returns>
+        private byte[] GetBytes(Icon icon)
+        {
+            MemoryStream ms = new MemoryStream();
+            icon.Save(ms);
+            return ms.ToArray();
+        }
+
+        private void BT_Convert_Click(object sender, EventArgs e)
+        {
+            bool flag = ConvertPicturePngToIco(imageInformation.FilePath, imageInformation.FileName);
+
+            if (flag)
+            {
+                LBL_Message.Text = "success";
+                LBL_Message.ForeColor = Color.Green;
+            }
+            else
+            {
+                LBL_Message.Text = "Error";
+                LBL_Message.ForeColor = Color.Red;
+            }
+        }
+
+        private void BT_OpenFolderDest_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", FolderMyPictures);
+        }
+    }
+
+    public class ImageInformation
+    {
+
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+
+        public ImageInformation() { }
+
+        public ImageInformation(string fileName, string filePath)
+        {
+            FileName = fileName;
+            FilePath = filePath;
+        }
+    }
+
+}
